599e988442d368f735ce9e70642b81c3d8a4c83d
DD <50895419+DD513@users.noreply.github.com>
2021-09-07 20:42:35 +0800

    cors laravel2

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: *");
-header("Access-Control-Allow-Headers: Origin, Methods, Content-Type");
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
