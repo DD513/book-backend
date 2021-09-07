9bcc972d111e97c02ac065da1ed99df8587d0a50
DD <50895419+DD513@users.noreply.github.com>
2021-09-07 20:36:26 +0800

    cors laravel

--- routes/api.php
+++ routes/api.php
@@ -24,10 +24,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/book', [BookController::class, 'getBook']);
-Route::post('/book', [BookController::class,'createBook']);
-Route::patch('/book/{id}', [BookController::class, 'editBook']);
-Route::delete('/book/{id}', [BookController::class, 'deleteBook']);
-Route::post('/thisbook', [BookController::class, 'findThisBook']);
-Route::get('/book/{id}', [BookController::class, 'getAssignBook']);
+Route::get('/book', [BookController::class, 'getBook'])->middleware("cors");;
+Route::post('/book', [BookController::class,'createBook'])->middleware("cors");;
+Route::patch('/book/{id}', [BookController::class, 'editBook'])->middleware("cors");;
+Route::delete('/book/{id}', [BookController::class, 'deleteBook'])->middleware("cors");;
+Route::post('/thisbook', [BookController::class, 'findThisBook'])->middleware("cors");;
+Route::get('/book/{id}', [BookController::class, 'getAssignBook'])->middleware("cors");;
 
